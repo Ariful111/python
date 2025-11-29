ec344c83fd43d8fdaf8535e59cd2b39109526389
Ariful111 <77139256+Ariful111@users.noreply.github.com>
2025-11-29 20:51:47 +0600

    Add files via upload

---
+++ function(1.1.10).py
@@ -0,0 +1,28 @@
+# built in input and print function
+name = input("input your name:")
+print("Hello", name) # Hello name
+
+print(int(3.14)) # 3
+print(int('42')) # 42
+print(int(True)) # 1
+print(int(False)) # 0
+print(str(45)) # '45'
+print(type(3.14)) # <class 'float'>
+print(isinstance(3, str)) # False
+
+# custom function
+def hello():
+    print("Hello World")
+hello() # Hello World
+
+def calculate_sum(a, b):
+    print(a+b)
+calculate_sum(3, 4) # 7
+my_sum = calculate_sum(7, 4) # 11
+print(my_sum) # None
+
+def calculate_sum_1(a, b):
+    return(a+b) # default return value is None
+calculate_sum_1(3, 4) # no outout
+my_sum_1 = calculate_sum_1(7, 4) # no output
+print(my_sum_1) # 11
